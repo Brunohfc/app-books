d08c45b5e41e9d370186a257de2297560c336948
Bruno Henrick <91575338+Brunohfc@users.noreply.github.com>
2025-07-14 18:53:31 -0300

    refatoracao, apenas 2 bottom navigation e renomeacao de DashboardFragment.kt para FavoritesFragment

--- app/src/main/java/com/example/books/ui/FavoritesFragment.kt
+++ app/src/main/java/com/example/books/ui/FavoritesFragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.books.databinding.FragmentNotificationsBinding
-import com.example.books.viewModels.NotificationsViewModel
+import com.example.books.databinding.FragmentFavoritesBinding
+import com.example.books.viewModels.FavoritesViewModel
 
-class NotificationsFragment : Fragment() {
+class FavoritesFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentFavoritesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,14 +23,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val favoritesViewModel =
+            ViewModelProvider(this).get(FavoritesViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textFavorites
+        favoritesViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
