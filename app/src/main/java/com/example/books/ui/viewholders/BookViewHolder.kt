25e66033de568bb9b2ba97d4e939d862f20299b1
Bruno Henrick <91575338+Brunohfc@users.noreply.github.com>
2025-07-19 22:55:11 -0300

    implementacao para customizar layout baseado nos dados da aplicacao(genero e favorito)

--- app/src/main/java/com/example/books/ui/viewholders/BookViewHolder.kt
+++ app/src/main/java/com/example/books/ui/viewholders/BookViewHolder.kt
@@ -2,16 +2,40 @@ package com.example.books.ui.viewholders
 
 
 import androidx.recyclerview.widget.RecyclerView
+import com.example.books.R
 import com.example.books.databinding.ItemsBookBinding
 import com.example.books.entities.BookEntity
 
 // vinculando o holder a view que desejo usar
-class BookViewHolder(private val item : ItemsBookBinding)
-    : RecyclerView.ViewHolder(item.root) {
+class BookViewHolder(private val item: ItemsBookBinding) : RecyclerView.ViewHolder(item.root) {
 
-        fun bind(book: BookEntity){
-            item.textViewBookTitle.text = book.title
-            item.textViewAuthor.text = book.author
-            item.textViewGenre.text = book.genre
+    fun bind(book: BookEntity) {
+        item.textViewBookTitle.text = book.title
+        item.textViewAuthor.text = book.author
+        item.textViewGenre.text = book.genre
+
+        setBackgroundColorOnGenre(item.textViewGenre.text.toString())
+        setFavoriteIcon(book.favorite)
+    }
+
+    private fun setFavoriteIcon(isFavorite: Boolean){
+        if(isFavorite){
+            item.imageViewFavorite.setImageResource(R.drawable.ic_favorite)
+        }else{
+            item.imageViewFavorite.setImageResource(R.drawable.ic_favorite_empty)
         }
+    }
+
+
+    private fun setBackgroundColorOnGenre(genre: String){
+        val TERROR = "Terror"
+        val ROMANCE = "Romance"
+        val FANTASIA = "Fantasia"
+        when(genre){
+            TERROR -> item.textViewGenre.setBackgroundResource(R.drawable.rounded_label_teal)
+            ROMANCE -> item.textViewGenre.setBackgroundResource(R.drawable.rounded_label_fantasy)
+            FANTASIA -> item.textViewGenre.setBackgroundResource(R.drawable.rounded_label_red)
+
+        }
+    }
 }
