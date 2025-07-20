5b874482e27ec8fc01dc88788c567f6406602824
Bruno Henrick <91575338+Brunohfc@users.noreply.github.com>
2025-07-19 23:40:59 -0300

    inclusao do fragmento de detalhe e cricao de layout do fragmento

---
+++ app/src/main/java/com/example/books/ui/viewholders/DetailFragment.kt
@@ -0,0 +1,32 @@
+package com.example.books.ui.viewholders
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.books.R
+import com.example.books.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: DetailViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailBinding.inflate(inflater, container,false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/books/ui/viewholders/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.books.ui.viewholders
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
